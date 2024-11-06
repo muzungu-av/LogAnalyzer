a0b3dd3f6851e686c4cf1e538f337a6a219dc7f3
Valera <val12042025@gmail.com>
2024-11-06 21:33:40 +0300

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
 import path from "path";
-import container from "./config/inversify/container";
-import { ILogger } from "./logger/logger.interface";
-import Controller from "./video/controller";
 import dotenv from "dotenv";
 
 const app = express();
@@ -11,23 +8,8 @@ const envPath = path.join(process.cwd(), `.env.production`);
 dotenv.config({ path: envPath });
 console.log(envPath);
 
-const logger = container.get<ILogger>("ILogger");
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  logger.info(`${req.method} ${req.url}`);
-  next();
-});
-
-logger.info("INFO logging level is enabled!");
-
 const port = 3000;
 
 app.use(express.static(path.join("./", "build")));
 
-const controller = container.get<Controller>("Controller");
-
-app.use("/", controller.getRoute());
-
-app.listen(port, () => {
-  logger.info(`Server Up and running on port ${port}`);
-});
+app.listen(port);
